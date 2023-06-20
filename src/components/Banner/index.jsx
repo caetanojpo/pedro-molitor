d664a9e4c45c35a6884df4c803fcfefd22d0e027
João Pedro Caetano <caetanojpo@gmail.com>
2023-06-19 22:38:46 -0300

    new implementations

--- src/components/Banner/index.jsx
+++ src/components/Banner/index.jsx
@@ -1,12 +1,12 @@
 import { Flex, Heading, Image } from "@chakra-ui/react";
 import banner from './banner.png'
-import pedro from './313983526_6159809457381420_7103292081667953991_n-removebg-preview.png'
+import pedro from './pedro-molitor.png'
 
 
 
 export default function Banner() {
     return (
-        <Flex h='80vh' w='100%' bgImage={banner} bgPosition='right' bgSize='contain'
+        <Flex as='section' h='80vh' w='100%' bgImage={banner} bgPosition='right' bgSize='fit'
             bgRepeat='no-repeat' justify='space-around' align='center'>
             <Flex flexDir='row' w='50%' paddingLeft='20px' justify='end'  >
                 <Flex flexDir='column' gap='8px' >
@@ -15,8 +15,8 @@ export default function Banner() {
                     <Heading fontFamily='Hysteria' fontSize='46px'>ESPECIALISTA EM ODONTO LEGAL</Heading>
                 </Flex>
             </Flex>
-            <Flex h='100%' w='50%' justify='end' align='end'>
-                <Image h='95%' src={pedro} />
+            <Flex h='100%' w='50%' justify='center' align='end'>
+                <Image h='100%' src={pedro} alt="Pedro Molitor, rapaz de cabelo preto, barba preta, oculos quadrado, camisa social preta, de estatura mediana, olhos escuros. Esta com os braços cruzados e sorriso no rosto." />
             </Flex>
         </Flex>
     )
